db3c8971cd14e9485501ed8fecec4cef0df3281b
Jarol Riera <80286893+HaroldTheCoder@users.noreply.github.com>
2022-02-16 18:43:16 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -23,3 +23,25 @@ mobileLink.forEach((i) => i.addEventListener('click', ()=> {
     activelayer.style.display = 'none';
     document.body.classList.remove('disable-scroll');
 }));
+
+// Email Validation
+const subForm = document.querySelector('.subscribe-form');
+const email = document.querySelector('.email-input');
+const errorMessage = document.querySelector('.error-message');
+const errorIcon = document.querySelector('.error-icon');
+const pattern = '[a-z0-9]+@[a-z]+\.[a-z]{2,3}';
+
+subForm.addEventListener('submit', (e)=> {
+    if(email.value.match(pattern)) {
+        errorMessage.style.opacity = '0';
+        errorIcon.style.opacity = '0';
+        console.log('Valid Email')
+        return true
+    } else {
+        e.preventDefault();
+        errorMessage.style.opacity = '1';
+        errorIcon.style.opacity = '1';
+        console.log('Invalid Email')
+        return false
+    }
+});
