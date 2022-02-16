a2974d0e8813c009b197e8d9360d008995289b16
Jarol Riera <80286893+HaroldTheCoder@users.noreply.github.com>
2022-02-16 12:20:06 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+// Mobile Navbar Function
+const openButton = document.querySelector('.nav-button');
+const closeButton = document.querySelector('.close-button');
+const navMobile = document.querySelector('.navbar-mobile');
+const activelayer = document.querySelector('.nav-active-layer')
+const mobileLink = document.querySelectorAll('.mobile-link');
+
+openButton.addEventListener('click', ()=> {
+    navMobile.classList.add('active');
+    navMobile.classList.remove('inactive');
+    activelayer.style.display = 'block';
+    document.body.classList.add('disable-scroll');
+});
+closeButton.addEventListener('click', ()=> {
+    navMobile.classList.add('inactive');
+    navMobile.classList.remove('active');
+    activelayer.style.display = 'none';
+    document.body.classList.remove('disable-scroll');
+});
+mobileLink.addEventListener('click', ()=> {
+    navMobile.classList.add('inactive');
+    navMobile.classList.remove('active');
+    activelayer.style.display = 'none';
+    document.body.classList.remove('disable-scroll');
+});
